b8884cec8b10b831702306591ea919319733452b
Gihan Harshana <gihanharshana42@gmail.com>
2025-04-03 09:56:19 +0530

    DBConnection

---
+++ NetBeansProjects/MVC-2/src/edu/ijse/mvc/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package edu.ijse.mvc;
+
+import edu.ijse.mvc.DBConnection.DBConnection;
+
+/**
+ *
+ * @author gihan5323
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception{
+        DBConnection.getInstance().getConnection();
+        // TODO code application logic here
+    }
+    
+}
